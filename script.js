d1a44d891a93c4a540b42f5f066209cb6c26c3a4
vipinvv <vipinvv555@gmail.com>
2021-03-05 19:00:59 +0530

    nav updated

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 function buttonClick(num) {
     console.log('number', num);
     document.getElementById('screen').value+=num;
+    var audio = new Audio( 
+    'sounds/num.mp3'); 
+    audio.play(); 
+                    
     
 }
 
 function clearDisplay() {
     document.getElementById('screen').value='';
+    var audio = new Audio( 'sounds/equ.mp3'); 
+    audio.play(); 
+
     
 }
 
 function back() {
     var value = document.getElementById("screen").value;
     document.getElementById("screen").value = value.substr(0, value.length - 1);
+    var audio = new Audio( 'sounds/equ.mp3'); 
+    audio.play(); 
 }
 
 function equelto() {
@@ -19,5 +28,9 @@ function equelto() {
     console.log('textttt', text);
     var result = eval(text)
     document.getElementById('screen').value = result;
+    var audio = new Audio( 'sounds/equ.mp3'); 
+    audio.play(); 
+                       
+
     
 }
